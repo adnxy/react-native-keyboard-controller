991c621ab83394f6028e5f5841422de8ea37fc84
kirillzyusko <zyusko.kirik@gmail.com>
2026-05-12 17:51:42 +0200

    fix: better way to get the actual instance of the keyboard

--- ios/observers/movement/KeyboardViewLocator.swift
+++ ios/observers/movement/KeyboardViewLocator.swift
@@ -7,15 +7,10 @@
 
 final class KeyboardViewLocator {
   static let shared = KeyboardViewLocator()
-  private var cachedKeyboardView: UIView?
+  private weak var cachedKeyboardView: UIView?
   private var cachedWindowsCount: Int = 0
 
   func resolve() -> UIView? {
-    // on iOS 26 we always get a new keyboard istance
-    if #available(iOS 26, *) {
-      return KeyboardView.find()
-    }
-
     let currentWindowsCount = UIApplication.shared.windows.count
     
     if cachedKeyboardView == nil || currentWindowsCount != cachedWindowsCount {
